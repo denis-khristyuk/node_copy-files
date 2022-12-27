dda658891ac029d79810e6aa8f781b7424244f99
denis-khristyuk <monsieur.apala@gmail.com>
2022-12-27 17:19:45 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const fs = require('fs');
+
+const copyFile = (file, copyPath) => {
+  if (file === copyPath) {
+    return;
+  }
+
+  const data = fs.readFileSync(file, 'utf8');
+
+  fs.writeFileSync(copyPath, data);
+};
+
+copyFile('./src/text.txt', './src/copiedText.txt');
